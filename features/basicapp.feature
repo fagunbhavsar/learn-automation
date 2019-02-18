84b2c84822208e77773b0655371b4c0c152f1ace
Fagun Bhavsar <fagunbhavsar@Faguns-MacBook-Air.local>
2019-02-18 17:08:05 +0530

    Modified files for refactoring the code

--- features/basicapp.feature
+++ features/basicapp.feature
@@ -9,35 +9,34 @@
     Then  I see a popup
     And   I click on Let's get started button
     Then  I see application panel
-    And   I see Basic tab is opened with following sections on left hand side column
-      | Hang Up or Exit    |
-      | Simple Prompt      |
-      | Multipart Prompt   |
-      | Jump to Page       |
-      | Simple Menu        |
-      | Transfer           |
-      | Yes or No          |
-      | Multipart + Yes/No |
-      | Digits Input       |
-      | Multiple Choice    |
-      | Voice Recording    |
-      | Label              |
+    #And   I see Basic tab is opened with following sections on left hand side column
+      #| Hang Up or Exit    |
+      #| Simple Prompt      |
+      #| Multipart Prompt   |
+      #| Jump to Page       |
+      #| Simple Menu        |
+      #| Transfer           |
+      #| Yes or No          |
+      #| Multipart + Yes/No |
+      #| Digits Input       |
+      #| Multiple Choice    |
+      #| Voice Recording    |
+      #| Label              |
     Then  I see Messaging tab on left hand side column
     And   I click on Messaging tab
-    Then  I see following sections
-      | Fetch Twitter Feed |
-      | Send an Email      |
-      | Send an SMS        |
+    #Then  I see following sections
+      #| Fetch Twitter Feed |
+      #| Send an Email      |
+      #| Send an SMS        |
     And   I see Start module on application panel
     Then  I click on New page button
     Then  I see New Page popup
     And   I enter name for "New App"
-    And   I click on create button
     Then  I click on Messaging tab
     Then  I drag Send an SMS module to application panel
     And   I see Sent and Not Sent ports
     And   I connect Start and Send an SMS modules
-    Then  I enter number for "Phone Number"
+    Then  I enter number for "123456789"
     And   I enter message for "Message Phrase"
     Then  I drag another component Send an Email to application panel
     And   I join Send an SMS component from Not Sent port to Send an Email component
--- features/steps/basicapp.rb
+++ features/steps/basicapp.rb
@@ -1,109 +1,110 @@
 Given("I open quick fuse app link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit(AppPage)
 end
 
 Then("I click on Create an app button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(AppPage).open_app_page
 end
 
 Then("I see a popup") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).view_pop_up
 end
 
 Then("I click on Let's get started button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).get_started
 end
 
 Then("I see application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+  is_app_panel_visible = on(EditPage).see_app_panel
+  expect(is_app_panel_visible).to eq(true)
 end
 
-Then("I see Basic tab is opened with following sections on left hand side column") do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
-end
+#Then("I see Basic tab is opened with following sections on left hand side column") do |table|
+
+#end
 
 Then("I see Messaging tab on left hand side column") do
-  pending # Write code here that turns the phrase above into concrete actions
+   is_messaging_tab_visible = on(EditPage).view_messaging_tab
+   expect(is_messaging_tab_visible).to eq(true)
 end
 
 Then("I click on Messaging tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+   on(EditPage).click_messaging_tab
 end
 
-Then("I see following sections") do |table|
+#Then("I see following sections") do |table|
   # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  #pending # Write code here that turns the phrase above into concrete actions
+#end
 
 Then("I see Start module on application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+  is_start_module_visible = on(EditPage).view_start_module
+  expect(is_start_module_visible).to eq(true)
 end
 
 Then("I click on New page button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).add_page
 end
 
 Then("I see New Page popup") do
-  pending # Write code here that turns the phrase above into concrete actions
+  pop_up = on(EditPage).see_newpage_popup
+  expect(pop_up).to eq(true)
 end
 
 Then("I enter name for {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I click on create button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).enter_new_name(string)
 end
 
 Then("I drag Send an SMS module to application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).module_send_n_sms
 end
 
 Then("I see Sent and Not Sent ports") do
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).sms_module_ports
 end
 
 Then("I connect Start and Send an SMS modules") do
-  pending # Write code here that turns the phrase above into concrete actions
+   on(EditPage).connect_sms_module
 end
 
 Then("I enter number for {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).set_phone_number(string)
 end
 
 Then("I enter message for {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  on(EditPage).set_message_text(string)
 end
 
 Then("I drag another component Send an Email to application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I join Send an SMS component from Not Sent port to Send an Email component") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I click on Basic tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I drag Hang Up or Exit component to application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I join Hang Up or Exit component from Sent port to Send an SMS component") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I drag another Hang up or Exit component to application panel") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I join Hang Up or Exit component from Not Sent port to Send an Email component") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Then("I join Hang Up or Exit component from Sent port to Send an Email Component") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
+
+
