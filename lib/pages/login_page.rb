22bac78698f62565b6c13f7fec24690f27a4b6f8
Fagun Bhavsar <fagunbhavsar@Faguns-MacBook-Air.local>
2019-02-24 11:49:49 +0530

    Removing other files not related to current project

--- lib/pages/login_page.rb
+++
@@ -1,9 +0,0 @@
-class LoginForm < Page
-
-  def login(username, password)
-    @browser.text_field(:id => 'user_login').set(username)
-    @browser.text_field(:id => 'user_password').set(password)
-    @browser.element(:value => 'Sign in').click
-  end
-
-end
